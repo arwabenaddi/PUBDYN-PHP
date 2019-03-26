dfbff6c03f6b0c068179df1b89008f95cd3ca3f5
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-26 15:05:14 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -16,42 +16,42 @@ $bucket = 'PUBDYN-PHPS3';
    // Instantiate the S3 client with your AWS credentials
 
     $s3 = new Aws\S3\S3Client([
-        'signature' => 'v2',
-        'version' => 'latest',
-        'region' => 'ap-southeast-1',
-        'credentials'  => [
-            'key'    => $CELLAR_ADDON_KEY_ID,
-            'secret' => $CELLAR_ADDON_KEY_SECRET
+        'signature'=>'v2',
+        'version'=>'latest',
+        'region'=>'ap-southeast-1',
+        'credentials'=>[
+            'key'=>$CELLAR_ADDON_KEY_ID,
+            'secret'=>$CELLAR_ADDON_KEY_SECRET
         ],
-        'endpoint' => "https://".$CELLAR_ADDON_HOST
+        'endpoint'=>"https://".$CELLAR_ADDON_HOST
       ]);
 
 
 // Use the high-level iterators (returns ALL of your objects).
 try {
     $results = $s3->getPaginator('ListObjects', [
-        'Bucket' => $bucket
+        'Bucket'=>$bucket
     ]);
 
     foreach ($results as $result) {
         foreach ($result['Contents'] as $object) {
-            echo $object['Key'] . PHP_EOL;
+            echo $object['Key'].PHP_EOL;
         }
     }
 } catch (S3Exception $e) {
-    echo $e->getMessage() . PHP_EOL;
+    echo $e->getMessage().PHP_EOL;
 }
 
 // Use the plain API (returns ONLY up to 1000 of your objects).
 try {
     $objects = $s3->listObjects([
-        'Bucket' => $bucket
+        'Bucket'=>$bucket
     ]);
     foreach ($objects['Contents']  as $object) {
-        echo $object['Key'] . PHP_EOL;
+        echo $object['Key'].PHP_EOL;
     }
 } catch (S3Exception $e) {
-    echo $e->getMessage() . PHP_EOL;
+    echo $e->getMessage().PHP_EOL;
 }
 
 ?>

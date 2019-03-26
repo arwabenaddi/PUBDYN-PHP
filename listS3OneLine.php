e89d6f1c735f3ab7c6f48abddc4d328f43d5c205
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-26 14:33:09 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -13,8 +13,8 @@ require_once $racine.'aws-autoloader.php';
 use Aws\S3\S3Client;
 
 $CELLAR_ADDON_HOST = getenv("CELLAR_ADDON_HOST");
-$CELLAR_ADDON_KEY_ID = getenv("CELLAR_ADDON_KEY_ID");
-$CELLAR_ADDON_KEY_SECRET = getenv("CELLAR_ADDON_KEY_SECRET");
+$CELLAR_ADDON_KEY_ID = getenv("SW016A92CMAJ79EUZY77");
+$CELLAR_ADDON_KEY_SECRET = getenv("KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6");
 //echo "\n".$CELLAR_ADDON_HOST."\n".$CELLAR_ADDON_KEY_ID."\n".$CELLAR_ADDON_KEY_SECRET."\n";
 
 try {

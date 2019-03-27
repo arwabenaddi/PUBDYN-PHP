3193b3308e827d32c7fff5a4d7c5108a7b95195a
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-27 18:58:37 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -54,16 +54,16 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
 // $keyname = 'buwd3fyvnjp7yxrdicdgtest.sql';
 
 try {
- $name = '';
+//  $name = '';
  $keyname = $s3->listObjects([
             'Bucket'=>$bucket       
     ]);
    foreach ($keyname['Contents']  as $object) {       
        echo $object['key'].PHP_EOL;       
-       $name = $object['key'] ; 
+//        $name = $object['key'] ; 
    }
  
-  echo '<br><br><br>'.$name;
+//   echo '<br><br><br>'.$name;
    //     // Get the object.
 //     $result = $s3->getObject([
 //         'Bucket' => $bucket,

7e02294a34c62992912b6f0cf431599a771e0541
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-27 11:33:55 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -63,13 +63,14 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
    foreach ($objects['Contents']  as $object) {
        
         echo $object['Key'].PHP_EOL;
-
+            $bd = $object['Key'].PHP_EOL;
+            $filebd = file_get_contents($bd);
+            echo 'contenu de fichier : ' $filebd;
    
    }
  } catch (S3Exception $e) {
     echo $e->getMessage().PHP_EOL;
  }
- echo  $object['Key'];
 
 
 

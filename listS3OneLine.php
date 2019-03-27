507beb35a5be4fed58b11dd674fbad9e0957a2d0
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-27 12:30:19 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -59,7 +59,7 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
     ]);
    foreach ($objects['Contents']  as $object) {
        
-        $read = readfile($object['Key'].PHP_EOL);
+        $object['Key'].PHP_EOL;
          
    
    }

5fbba6b640335d7aa4429115633b780c29ebb05e
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-28 11:02:42 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -84,7 +84,7 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
      // Temporary variable, used to store current query
    
    // Read in entire file
-   $filename = $result;
+   $filename = $result['Body'];
    //  echo $contents;
    //  $contents = str_replace(' ','',$contents);
    // $lines = str_replace(CHR(13).CHR(10),"",$contents);

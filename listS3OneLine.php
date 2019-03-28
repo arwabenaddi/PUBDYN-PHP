8199305db687856cfdac0d8ce0a69f1763a935ff
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-28 17:20:47 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -32,7 +32,7 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
       'Bucket'=>$bucket       
     ]);
    foreach ($objects['Contents']  as $object) {       
-        echo $object['Key'].PHP_EOL;
+//         echo $object['Key'].PHP_EOL;
         $name =  $object['Key'];           
    }  
     // Get the object.
@@ -45,6 +45,7 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
 //echo $result['Body'];
        $contents = $result['Body'];
        $content = str_replace("arwa","test",$contents);      
+       $object['Key'] = $content;
 //echo $content;
     $connection=mysqli_connect($host,$dbUsername,$dbPass,$dbname);
     if (mysqli_connect_error()){
@@ -54,33 +55,18 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
 // Temporary variable, used to store current query
 $templine = '';
 // Read in entire file
+       echo $object['Key'];
 $filename = $object['Key'];
-
-//  echo $contents;
-//  $contents = str_replace(' ','',$contents);
-
-// $lines = str_replace(CHR(13).CHR(10),"",$contents);
-
  $lines = file($filename);
 // Loop through each line
 foreach ($lines as $line)
-{
-   
+{   
  echo "dans la boucle"; 
-// Skip it if it's a comment
-// || $line == str_replace(CHR(13).CHR(10),"",$line) 
 if (substr($line,0,2) == '--' || $line == '' )
     continue;
-
-// Add this line to the current segment
 $templine = $line;
-
-// If it has a semicolon at the end, it's the end of the query
 if (substr(trim($line), -1, 1) == ';')
 {
-    // $line = str_replace(CHR(13).CHR(10),"",$line);
-    // Perform the query
-    // $insertfile = "INSERT INTO db VALUES ($templine)";
     mysqli_query($connection,$templine) or die('Erreur insertion file'.$templine.'<br>'.mysqli_error($connection));
     // Reset temp variable to empty
     $templine = '';

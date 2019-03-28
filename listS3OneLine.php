5c96f327986a5d70f75e1fe0a65279d16671df0a
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-28 14:43:31 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -88,7 +88,7 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
 
 // Insertion fichier dans la BD
 // Temporary variable, used to store current query
-        $contents = $result['Body'];
+       $contents = $result['Body'];
         // // Read in entire file
         // $filename = $name;
         // $lines = file($filename);
@@ -97,35 +97,35 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
        $content = str_replace("arwa","test",$contents);
 //       $lines = str_replace("arwa","test",$contents);
       
-      echo $content;
-        // //     $lines = $filename;
+       echo $content;
+        
         // // Loop through each line
-        // $templine = "";
+        $templine = "";
 
-        // if (!empty($lines)){
-        //     echo "function iffff";
-        //     foreach ($lines as $line)
-        //     {
-        //             echo 'cccccccccccccccccccccc';
-        //         // Skip it if it's a comment
+       
+        
+        foreach ($content as $line)
+        {
+                    echo 'cccccccccccccccccccccc';
+                // Skip it if it's a comment
         //         // || $line == str_replace(CHR(13).CHR(10),"",$line) 
-        //         if (substr($line,0,2) == '--' || $line == '' )
-        //             continue;
+                if (substr($line,0,2) == '--' || $line == '' )
+                     continue;
         //         // Add this line to the current segment
-        //         $templine = $line;
+               $templine = $line;
         //         // If it has a semicolon at the end, it's the end of the query
-        //         if (substr(trim($line), -1, 1) == ';')
-        //         {
-        //             // $line = str_replace(CHR(13).CHR(10),"",$line);
+               if (substr(trim($line), -1, 1) == ';')
+                 {
+                    // $line = str_replace(CHR(13).CHR(10),"",$line);
         //             // Perform the query
         //             // $insertfile = "INSERT INTO db VALUES ($templine)";
-        //             echo 'insert to database';
-        //             mysqli_query($connection,$templine) or die('Erreur insertion file'.$templine.'<br>'.mysqli_error($connection));
+                   echo 'insert to database';
+                    mysqli_query($connection,$templine) or die('Erreur insertion file'.$templine.'<br>'.mysqli_error($connection));
         //             // Reset temp variable to empty
-        //             $templine = "";
-        //         }
-        //     }
-        // }
+                   $templine = "";
+             
+            }
+         }
   
  } 
  catch (S3Exception $e) {

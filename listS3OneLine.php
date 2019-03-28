56af03027f85d56f314f9f6b724136ed2461c651
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-28 17:13:40 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -51,11 +51,42 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
         die('connect Error ('.mysqli_connect_error().')'.mysqli_connect_error());
     }
     else{
-       mysqli_query($connection,$object['Key']) or die('Erreur insertion file'.$object['Key'].'<br>'.mysqli_error($connection));
-       else 
-       { 
-           echo "Echec de l'enregistrement, désolé"; 
-       }  
+// Temporary variable, used to store current query
+$templine = '';
+// Read in entire file
+$filename = $object['Key'];
+
+//  echo $contents;
+//  $contents = str_replace(' ','',$contents);
+
+// $lines = str_replace(CHR(13).CHR(10),"",$contents);
+
+ $lines = file($filename);
+// Loop through each line
+foreach ($lines as $line)
+{
+   
+ echo "dans la boucle"; 
+// Skip it if it's a comment
+// || $line == str_replace(CHR(13).CHR(10),"",$line) 
+if (substr($line,0,2) == '--' || $line == '' )
+    continue;
+
+// Add this line to the current segment
+$templine = $line;
+
+// If it has a semicolon at the end, it's the end of the query
+if (substr(trim($line), -1, 1) == ';')
+{
+    // $line = str_replace(CHR(13).CHR(10),"",$line);
+    // Perform the query
+    // $insertfile = "INSERT INTO db VALUES ($templine)";
+    mysqli_query($connection,$templine) or die('Erreur insertion file'.$templine.'<br>'.mysqli_error($connection));
+    // Reset temp variable to empty
+    $templine = '';
+}
+}
+ echo "Tables imported successfully"; 
    } 
    
 } 

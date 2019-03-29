e6fdb3b6b2f8c4c5649eaa68405d94b447678f29
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-29 17:03:12 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -51,7 +51,7 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
   $text = 'SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";SET AUTOCOMMIT = 0;START TRANSACTION;SET time_zone = "+00:00";';
              $contents = $result['Body'];
              $content = str_replace("arwa","test",$contents); 
-             $content = str_replace($text,"",$content); 
+             $content = preg_replace($text,"",$content); 
              $tests = preg_replace("#(--).*(\n)#", "", $content);
              echo $tests;
   if (mysqli_connect_error()){

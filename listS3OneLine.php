aeb1bc9f459fe43cb2a5acb289053d1cb6586dbb
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-29 17:01:04 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -48,9 +48,10 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
 
       //Affichage contenu de fichier existant dans le bucket 
       //echo $result['Body'];
+  $text = 'SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";SET AUTOCOMMIT = 0;START TRANSACTION;SET time_zone = "+00:00";';
              $contents = $result['Body'];
              $content = str_replace("arwa","test",$contents); 
-             $content = str_replace('SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";SET AUTOCOMMIT = 0;START TRANSACTION;SET time_zone = "+00:00";','',$content); 
+             $content = str_replace($text,"",$content); 
              $tests = preg_replace("#(--).*(\n)#", "", $content);
              echo $tests;
   if (mysqli_connect_error()){

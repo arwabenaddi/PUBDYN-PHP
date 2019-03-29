62b67ff9c74ec20fdb39b8daaede991963fa245f
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-29 11:51:23 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -45,13 +45,13 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
 //echo $result['Body'];
        $contents = $result['Body'];
        $content = str_replace("arwa","test",$contents);  
-        echo $content;
+//         echo $content;
  $connection=mysqli_connect($host,$dbUsername,$dbPass,$dbname);
 if (mysqli_connect_error()){
     die('connect Error ('.mysqli_connect_error().')'.mysqli_connect_error());
 }
 else{
-    mysqli_query($connection,$content) or die('Erreur insertion file'.$content.'<br>'.mysqli_error($connection));
+    mysqli_query($connection,'`'+$content+'`') or die('Erreur insertion file'.$content.'<br>'.mysqli_error($connection));
 
 }
  } catch (S3Exception $e) {

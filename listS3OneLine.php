33793ec2d5d4aaaff3f09cc4dbb7052f7f6831f5
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-03-29 12:34:42 +0100

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -50,9 +50,33 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
 if (mysqli_connect_error()){
     die('connect Error ('.mysqli_connect_error().')'.mysqli_connect_error());
 }
- echo $name;
- echo $result['Body'];
-   
+
+$templine = '';
+// Read in entire file
+$filename = $name;
+ $lines = file($filename);
+// Loop through each line
+foreach ($lines as $line)
+{
+// Skip it if it's a comment
+// || $line == str_replace(CHR(13).CHR(10),"",$line) 
+if (substr($line,0,2) == '--' || $line == '' )
+    continue;
+
+// Add this line to the current segment
+$templine = $line;
+
+// If it has a semicolon at the end, it's the end of the query
+if (substr(trim($line), -1, 1) == ';')
+{
+    // $line = str_replace(CHR(13).CHR(10),"",$line);
+    // Perform the query
+    // $insertfile = "INSERT INTO db VALUES ($templine)";
+    mysqli_query($connection,$templine) or die('Erreur insertion file'.$templine.'<br>'.mysqli_error($connection));
+    // Reset temp variable to empty
+    $templine = '';
+}
+}
  
 
 

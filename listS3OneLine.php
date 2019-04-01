ec7ddcf12c498eef30454635dd4eed9fe5076338
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-04-01 15:14:10 +0200

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -71,48 +71,53 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
              
          ]);
          
-         
+  echo $resultput['body'];
+  
+//          // Temporary variable, used to store current query
+// $templine = '';
+
+// // Loop through each line
+// foreach ($lines as $line)
+// {
+// // Skip it if it's a comment
+// // || $line == str_replace(CHR(13).CHR(10),"",$line) 
+// if (substr($line,0,2) == '--' || $line == '' )
+//     continue;
+
+// // Add this line to the current segment
+// $templine = $line;
+
+// // If it has a semicolon at the end, it's the end of the query
+// if (substr(trim($line), -1, 1) == ';')
+// {
+//     // $line = str_replace(CHR(13).CHR(10),"",$line);
+//     // Perform the query
+//     // $insertfile = "INSERT INTO db VALUES ($templine)";
+//     mysqli_query($connection,$templine) or die('Erreur insertion file'.$templine.'<br>'.mysqli_error($connection));
+//     // Reset temp variable to empty
+//     $templine = '';
+// }
+// }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
           if (mysqli_connect_error()){
              die('connect Error ('.mysqli_connect_error().')'.mysqli_connect_error());
          }
        else {
         
-           
-// Temporary variable, used to store current query
-$templine = '';
-// Read in entire file
-$filename = $name;
-// $filenames = readfile('db.sql'); 
-//  echo $contents;
-//  $contents = str_replace(' ','',$contents);
-
-// $lines = str_replace(CHR(13).CHR(10),"",$contents);
-echo $filename;
- $lines = file($filename);
-        echo $lines;
-// Loop through each line
-foreach ($lines as $line)
-{
-// Skip it if it's a comment
-// || $line == str_replace(CHR(13).CHR(10),"",$line) 
-if (substr($line,0,2) == '--' || $line == '' )
-    continue;
-
-// Add this line to the current segment
-$templine = $line;
-
-// If it has a semicolon at the end, it's the end of the query
-if (substr(trim($line), -1, 1) == ';')
-{
-    // $line = str_replace(CHR(13).CHR(10),"",$line);
-    // Perform the query
-    // $insertfile = "INSERT INTO db VALUES ($templine)";
-    mysqli_query($connection,$templine) or die('Erreur insertion file'.$templine.'<br>'.mysqli_error($connection));
-    // Reset temp variable to empty
-    $templine = '';
-}
-}
- echo "Tables imported successfully";
+         echo "Tables imported successfully";
 
         }
     

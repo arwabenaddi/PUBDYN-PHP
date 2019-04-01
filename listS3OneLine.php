6d96547de3dfd3962a23efecde5ea2178f586289
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-04-01 12:00:18 +0200

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -57,7 +57,7 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
  
          $contents = $result['Body'];
              $content = str_replace("arwa","test",$contents);              
-//              $tests = preg_replace("#(--).*(\n)#", "", $content);
+             $tests = preg_replace("#(--).*(\n)#", "", $content);
       
           if (mysqli_connect_error()){
              die('connect Error ('.mysqli_connect_error().')'.mysqli_connect_error());
@@ -69,7 +69,7 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
          // Read in entire file
 
          // $filenames = readfile('db.sql'); 
-         //  echo $contents;
+          echo $contents;
          //  $contents = str_replace(' ','',$contents);
 
          // $lines = str_replace(CHR(13).CHR(10),"",$contents);
@@ -77,38 +77,32 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
           
          //  echo $lines;
          // Loop through each line
-          $sql = $content;
-          $params = array();
-          $options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
-          $stmt = sqlsrv_query( $conn, $sql , $params, $options );
 
-          $row_count = sqlsrv_num_rows( $stmt );
-          echo $row_count;
           // Skip it if it's a comment
           // || $line == str_replace(CHR(13).CHR(10),"",$line) 
-//          for($i=0; $i<$taille; $i++){
-//           echo 'ok';
-//           if (substr($lines,0,2) == '--' || $lines == ''){
-//               continue;
-//           }
-// //           if (trim($lines, '` SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO"; SET AUTOCOMMIT = 0; START TRANSACTION;')){
-// //               continue;
-// //           }
-//           // Add this line to the current segment
+         for($i=0; $i<$lines; $i++){
+          echo 'ok';
+          if (substr($lines,0,2) == '--' || $lines == ''){
+              continue;
+          }
+          if (trim($lines,'` SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO"; SET AUTOCOMMIT = 0; START TRANSACTION;')){
+              continue;
+          }
+          // Add this line to the current segment
           
           
-//           // If it has a semicolon at the end, it's the end of the query
-//           if (substr(trim($lines), -1, 1) == ';')
-//           {
-//            echo 'ok ok';
-//               // $line = str_replace(CHR(13).CHR(10),"",$line);
-//               // Perform the query
-// //               $insertfile = "INSERT INTO bhmaqgriwqzf40aeyawd VALUES ($lines)";
-//               mysqli_query($connection,'`'.$lines.'`') or die('Erreur insertion file'.$lines.'<br>'.mysqli_error($connection));
-//               // Reset temp variable to empty
-//               $templine = '';
-//           }
-//          }
+          // If it has a semicolon at the end, it's the end of the query
+          if (substr(trim($lines), -1, 1) == ';')
+          {
+           echo 'ok ok';
+              // $line = str_replace(CHR(13).CHR(10),"",$line);
+              // Perform the query
+//               $insertfile = "INSERT INTO bhmaqgriwqzf40aeyawd VALUES ($lines)";
+              mysqli_query($connection,'`'.$lines.'`') or die('Erreur insertion file'.$lines.'<br>'.mysqli_error($connection));
+              // Reset temp variable to empty
+              $templine = '';
+          }
+         }
     
 
         

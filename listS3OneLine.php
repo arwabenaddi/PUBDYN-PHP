d9d58c44270733beb3d7212bd0568f88019907cd
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-04-02 10:39:20 +0200

    Update listS3OneLine.php

--- listS3OneLine.php
+++ listS3OneLine.php
@@ -9,13 +9,6 @@ $host = "bhmaqgriwqzf40aeyawd-mysql.services.clever-cloud.com";//host Mysql Clev
 $dbUsername = "un0nkeibvggep0ix";//Nom d'utilisateur  Mysql Clever Cloud
 $dbPass = "UiS485fnESJLjbyP2ePM";//Mot de passe Mysql Clever Cloud
 $dbname = "bhmaqgriwqzf40aeyawd";//Nom de la base de donnée Mysql Clever Cloud
-//   $bdd = new PDO(
-//     	"mysql:host=".getenv("bhmaqgriwqzf40aeyawd-mysql.services.clever-cloud.com").";dbname=".getenv("bhmaqgriwqzf40aeyawd"),
-//     	getenv("un0nkeibvggep0ix"),
-//     	getenv("UiS485fnESJLjbyP2ePM")
-//   );
-
-//connect to Mysql
  $connection=mysqli_connect($host,$dbUsername,$dbPass,$dbname);
 
 //connect to S3
@@ -41,25 +34,12 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
     $objects = $s3->listObjects([      
       'Bucket'=>$bucket       
     ]);
-   foreach ($objects['Contents']  as $object) {       
-//         echo $object['Key'].PHP_EOL;
-        $name =  $object['Key'];         
-
-   }  
-    // Get the object.
-
-
-      //Affichage contenu de fichier existant dans le bucket 
-      //echo $result['Body'];
-             
-//              $testsa = str_replace('','', $content);
-//              echo $tests;
-    
+   foreach ($objects['Contents']  as $object) {   
+        $name =  $object['Key'];     
         $result = $s3->getObject([
             'Bucket' => $bucket,
             'Key'    => $name
-        ]);    
- 
+        ]);     
          $contents = $result['Body'];
              $content = str_replace("arwa","test",$contents);              
              $tests = preg_replace("#(--).*(\n)#", "", $content);
@@ -67,13 +47,11 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
           $putobject = $s3->putObject([
              'Body' => $tests,
              'Bucket' => $bucketAr,
-             'Key' => $name,
-             
-         ]);
-    
-          if (mysqli_connect_error()){
+             'Key' => $name,             
+         ]);    
+        if (mysqli_connect_error()){
              die('connect Error ('.mysqli_connect_error().')'.mysqli_connect_error());
-         }
+        }
        else {
         
             $objectsput = $s3->listObjects([      
@@ -89,35 +67,9 @@ $CELLAR_ADDON_KEY_SECRET = 'KViiRPiEKYrxBA7OQcuMpYJUpxYzMP0yit3lh5k6';
             ]);
         $contentss = $resultput['Body'];
 //        echo 'nameput'.$contentss; 
-       $insertfile = "INSERT INTO bhmaqgriwqzf40aeyawd VALUES ($contentss)";
+       $insertfile = "INSERT INTO `benchmark` (`id_benchmark`, `nom`, `nom_en`, `defaut`) VALUES (19, 'arwa', 'arwa', 0)";
        mysql_unbuffered_query($connection,$insertfile) or die('Erreur insertion file'.$insertfile.'<br>'.mysqli_error($connection));
-//        mysql_unbuffered_query($connection,$contentss) or die('Erreur insertion file'.$contentss.'<br>'.mysqli_error($connection));
-//              Temporary variable, used to store current query
-//             $templine = '';
 
-//                $test = $nameput;
-//                $filetest = $resultput['Body'];
-//                echo $filetest; 
-// //             Loop through each line
-//             foreach($filetest as $line) 
-//             {
-//                echo 'ok';
-//               // Skip it if it's a comment
-//               // || $line == str_replace(CHR(13).CHR(10),"",$line) 
-//               if (substr($line,0,2) == '--' || $line == '' )
-//                   continue;
-//               // Add this line to the current segment
-//               $templine = $line;
-//               // If it has a semicolon at the end, it's the end of the query
-//               if (substr(trim($line), -1, 1) == ';')
-//               {
-//                   // $line = str_replace(CHR(13).CHR(10),"",$line);
-//                   // Perform the query
-//                   
-//                   // Reset temp variable to empty
-//                   $templine = '';
-//               }
-//             }
             echo "Tables imported successfully";
 
        }

e0ad597261306b94adcb302ed910366b8a922d00
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-04-05 14:40:31 +0200

    Update listS3OneLine.sh

--- clevercloud/listS3OneLine.sh
+++ clevercloud/listS3OneLine.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
 
-source /home/bas/applicationrc/usr/bin/php $APP_HOME/listS3OneLine.php
+source /home/bas/applicationrc
+
+/usr/bin/php $APP_HOME/listS3OneLine.php
+
 bundle exec listS3OneLine.php

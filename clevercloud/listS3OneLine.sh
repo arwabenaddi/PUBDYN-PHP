81e7d9a48ff7b6e425a410b95ff1d7d9f1b314f8
arwabenaddi <35273429+arwabenaddi@users.noreply.github.com>
2019-04-05 11:43:07 +0200

    Update listS3OneLine.sh

--- clevercloud/listS3OneLine.sh
+++ clevercloud/listS3OneLine.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-source /home/bas/app_81cd652f-c489-40d7-bd9a-137afd06953a
+source /home/bas/applicationrc
 
 /usr/bin/php $APP_HOME/listS3OneLine.php
 
-echo "okbatch"
+echo 'okay bash'
